a42a7bc96f2a3afcd2170766922e4a05350e678e
Jim Chen <teioh31@hotmail.com>
2017-04-10 11:34:23 -0700

    1st commit

---
+++ step_test_with_SK/mapScene.swift
@@ -0,0 +1,9 @@
+//
+//  mapScene.swift
+//  step_test_with_SK
+//
+//  Created by Owner on 3/29/17.
+//  Copyright © 2017 Owner. All rights reserved.
+//
+
+import Foundation
---
+++ step_test_with_SK/questScene.swift
@@ -0,0 +1,9 @@
+//
+//  questScene.swift
+//  step_test_with_SK
+//
+//  Created by Owner on 3/31/17.
+//  Copyright © 2017 Owner. All rights reserved.
+//
+
+import Foundation
